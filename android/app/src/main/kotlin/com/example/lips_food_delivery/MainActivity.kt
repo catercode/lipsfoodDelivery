773f2bae9273d561528988ba2fbb963d1806291b
catercode <catercode00@gmail.com>
2022-03-15 03:36:38 -0800

    lIPS

---
+++ android/app/src/main/kotlin/com/example/lips_food_delivery/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.lips_food_delivery
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+import 'package:device_preview/device_preview.dart';
+import 'package:lips_food_delivery/pages/home_page.dart';
+
+void main() => runApp(
+      DevicePreview(
+        enabled: true,
+        builder: (context) => const MyApp(), // Wrap your app
+      ),
+    );
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      useInheritedMediaQuery: true,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
